97e6e1c26146b82ad78a6937208fd7a29787f7b4
santoshkumarchalumuri <129928042+santoshkumarchalumuri@users.noreply.github.com>
2023-04-05 12:28:07 +0530

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,17 @@
+import javax.xml.transform.stream.StreamSource;
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("eneter a number:");
+        int num = sc.nextInt();
+        if(num%2==0){
+            System.out.println("even number");
+        }
+        else {
+            System.out.println("odd number");
+        }
+
+    }
+}
